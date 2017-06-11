213e03f8716f2eff3bf8f0f574532019474f9a1e
ControlDy <791219648@qq.com>
2017-06-11 12:02:35 +0800

    add resource.h

---
+++ Classes/resource.h
@@ -0,0 +1,15 @@
+#ifndef RESOURCE
+#define RESOURCE
+
+//////////////////////////////////////////////////////////////////
+static const char IMG_OBSTACLE[] = "obstacle/hollow_circle.png";
+
+static const int TAG_BALL= 1000;
+static const int TAG_OBSTACLE = 1001;
+
+static const int GAME_STATUS_START = 10;
+static const int GAME_STATUS_PLAYING = 20;
+static const int GAME_STATUS_GAME_OVER = 30;
+static const int GAME_STATUS_RESTART = 40;
+
+#endif
