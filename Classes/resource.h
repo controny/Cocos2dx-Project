2650a357271039bb38d9c7d81e8758af6fafbb87
ControlDy <791219648@qq.com>
2017-06-11 21:55:47 +0800

    change some variable names

--- Classes/resource.h
+++ Classes/resource.h
@@ -11,25 +11,21 @@ static const int TAG_BALL= 1000;
 static const int TAG_OBSTACLE = 1001;
 static const int TAG_PROP = 1002;
 
+// 以下是根据旋转角度判断触碰颜色的参数
 const int Section = 72;
 const int Half_Section = 36;
 const int Circle = 360;
-// (rotation >= 0 && rotation < COLOR_SHUI_MIN) ||  rotation >= COLOR_SHUI_MAX )
-static const int COLOR_SHUI_MIN = Half_Section;
-static const int COLOR_SHUI_MAX = Circle - Half_Section;
-// ( rotation >= COLOR_XX_MIN && rotation < COLOR_XX_MAX )
-static const int COLOR_MU_MIN =Half_Section;
-static const int COLOR_MU_MAX = COLOR_MU_MIN + Section;
-static const int COLOR_JIN_MIN = Half_Section + Section;
-static const int COLOR_JIN_MAX = COLOR_JIN_MIN + Section;
-static const int COLOR_HUO_MIN = Half_Section + Section * 2;
-static const int COLOR_HUO_MAX = COLOR_HUO_MIN + Section;
-static const int COLOR_TU_MIN = Half_Section + Section * 3;
-static const int COLOR_TU_MAX = COLOR_TU_MIN + Section;
-
-static const int GAME_STATUS_START = 10;
-static const int GAME_STATUS_PLAYING = 20;
-static const int GAME_STATUS_GAME_OVER = 30;
-static const int GAME_STATUS_RESTART = 40;
+// (rotation >= 0 && rotation < CIRCLE_SHUI_MIN) ||  rotation >= CIRCLE_SHUI_MAX )
+static const int CIRCLE_SHUI_MIN = Half_Section;
+static const int CIRCLE_SHUI_MAX = Circle - Half_Section;
+// ( rotation >= CIRCLE_XX_MIN && rotation < CIRCLE_XX_MAX )
+static const int CIRCLE_MU_MIN =Half_Section;
+static const int CIRCLE_MU_MAX = CIRCLE_MU_MIN + Section;
+static const int CIRCLE_JIN_MIN = Half_Section + Section;
+static const int CIRCLE_JIN_MAX = CIRCLE_JIN_MIN + Section;
+static const int CIRCLE_HUO_MIN = Half_Section + Section * 2;
+static const int CIRCLE_HUO_MAX = CIRCLE_HUO_MIN + Section;
+static const int CIRCLE_TU_MIN = Half_Section + Section * 3;
+static const int CIRCLE_TU_MAX = CIRCLE_TU_MIN + Section;
 
 #endif
